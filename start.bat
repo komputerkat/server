f42ce72a08c6077d30ddb61e616224761566bc92
42MonthsAfter <42monthsafter@gmail.com>
2021-04-18 23:34:22 -0400

    2021.04.18 Update

--- start.bat
+++ start.bat
@@ -1,2 +1,5 @@
 @echo off
-start DayZServer_x64.exe -config=serverDZ.cfg -port=2302 -mod=@CF;@CommunityOnlineTools;@DayZEditorLoader;@42MonthsAfter;@CodeLock;@zSpawnSelection;@ZTVendingMachine;@KillFeed;@AirSoftGuns;@TranquilizerGun;@RubberBullets;@ToFuAirsoftGuns;@MasssManyItemOverhaul;@CannabisPlus;@MunghardsItempack;@ZeroyFishingZ;@NatureOverhaul;@BuilderItems;@ModularVestSystem;@BaseBuildingPlus;@MortysWeapons;@EarPlugs;@BuildAnywhere_v3;@AirdropUpgraded;@ToxicZone;@TeddysWeaponPack;@Dartguns;@VanillaPlusPlusMap;@IRP-Land-Rover-Defender-110;@Trader; -profiles=ServerProfile -dologs -adminlog -netlog
+:START
+DayZServer_x64.exe -config=serverDZ.cfg -port=2302 -mod=@CF;@CommunityOnlineTools;@DayZEditorLoader;@42MonthsAfter;@CodeLock;@zSpawnSelection;@ZTVendingMachine;@KillFeed;@AirSoftGuns;@TranquilizerGun;@RubberBullets;@ToFuAirsoftGuns;@MasssManyItemOverhaul;@CannabisPlus;@MunghardsItempack;@ZeroyFishingZ;@NatureOverhaul;@BuilderItems;@ModularVestSystem;@BaseBuildingPlus;@MortysWeapons;@EarPlugs;@BuildAnywhere_v3;@AirdropUpgraded;@ToxicZone;@TeddysWeaponPack;@Dartguns;@VanillaPlusPlusMap;@IRP-Land-Rover-Defender-110;@Trader; -profiles=ServerProfile -dologs -adminlog -netlog
+timeout 30
+goto START
